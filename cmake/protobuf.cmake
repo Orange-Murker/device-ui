3ad45e437deafa245f5ec6bc5cbd63789362be17
Orange-Murker <23561952+Orange-Murker@users.noreply.github.com>
2025-08-24 14:16:12 +0200

    feat: update protobuf

--- cmake/protobuf.cmake
+++ cmake/protobuf.cmake
@@ -2,6 +2,6 @@ message(STATUS "Fetching protobufs ...")
 FetchContent_Declare(
     Protobuf
     GIT_REPOSITORY "https://github.com/meshtastic/protobufs"
-    GIT_TAG  v2.6.2
+    GIT_TAG  v2.6.11
 )
 FetchContent_MakeAvailable(Protobuf)
